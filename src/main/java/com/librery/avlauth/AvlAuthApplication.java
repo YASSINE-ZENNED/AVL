4e73e96ac9b81d64793c35434a616b6a9ef30284
YASSINE ZENNED <yassine.zenned.123@gmail.com>
2024-10-02 13:45:39 +0100

    ship to github

---
+++ src/main/java/com/librery/avlauth/AvlAuthApplication.java
@@ -0,0 +1,13 @@
+package com.librery.avlauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvlAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvlAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/librery/avlauth/AvlAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librery.avlauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvlAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
